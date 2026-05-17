c9354a11a6a72d9a677647c21b9bcbe20f08fbdd
easinchat <easin.chat@email.com>
2026-05-17 17:28:32 +0600

    requesting API to get json data and using forEach get one by one

--- script.js
+++ script.js
@@ -13,6 +13,22 @@
 // const parsJson = JSON.parse(personJson);
 // console.log(parsJson, typeof parsJson);
 
-const result = fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const loadData = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
+
+const loadPost = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((res) => res.json())
+
+    .then((data) => {
+      console.log(data);
+      displayPost(data);
+    });
+};
+const displayPost = (posts) => {
+  posts.forEach((post) => console.log(post));
+};
