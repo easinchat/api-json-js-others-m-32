467e43970e997dbf1da5b72a1a2fc9c4db3e42ba
easinchat <easin.chat@email.com>
2026-05-17 12:32:36 +0600

    object to json and json to object  first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const person = {
+  name: "Easin",
+  age: 23,
+  food: "kacci Biriyani",
+  isRich: false,
+  money: 1000,
+};
+console.log(person, typeof person);
+//
+const personJson = JSON.stringify(person);
+console.log(personJson, typeof personJson);
+//
+const parsJson = JSON.parse(personJson);
+console.log(parsJson, typeof parsJson);
