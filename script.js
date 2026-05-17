5a42445801c0a2f6177e3f57d1954c0be0e8171f
easinchat <easin.chat@email.com>
2026-05-17 16:18:08 +0600

    about fetch

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const person = {
-  name: "Easin",
-  age: 23,
-  food: "kacci Biriyani",
-  isRich: false,
-  money: 1000,
-};
-console.log(person, typeof person);
-//
-const personJson = JSON.stringify(person);
-console.log(personJson, typeof personJson);
-//
-const parsJson = JSON.parse(personJson);
-console.log(parsJson, typeof parsJson);
+// const person = {
+//   name: "Easin",
+//   age: 23,
+//   food: "kacci Biriyani",
+//   isRich: false,
+//   money: 1000,
+// };
+// console.log(person, typeof person);
+// //
+// const personJson = JSON.stringify(person);
+// console.log(personJson, typeof personJson);
+// //
+// const parsJson = JSON.parse(personJson);
+// console.log(parsJson, typeof parsJson);
+
+const result = fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
